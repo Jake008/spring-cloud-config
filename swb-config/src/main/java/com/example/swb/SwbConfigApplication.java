30b6a3e571c3079b4e48a3c9fe6bac34c7eb194b
luqiang <1575709512@qq.com>
2020-01-15 22:01:56 +0800

    qq

--- swb-config/src/main/java/com/example/swb/SwbConfigApplication.java
+++ swb-config/src/main/java/com/example/swb/SwbConfigApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class SwbConfigApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SwbConfigApplication.class, args);
     }
